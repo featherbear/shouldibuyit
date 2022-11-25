b93999c1f5aab4ea49f0264a1f62c0d273f3c96c
Andrew Wong <andrew.j.wong@outlook.com>
2022-11-25 18:52:19 +1100

    Yeah

---
+++ src/lib/Question.svelte
@@ -0,0 +1,13 @@
+<div class="content">
+  <slot />
+</div>
+<div class="divider" />
+
+<style lang="scss">
+  .content {
+    // transition: background-color 0.3s;
+    // &:hover {
+    //     background-color: hsl(var(--b3));
+    // }
+  }
+</style>

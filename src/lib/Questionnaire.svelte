d46516462e3629cc2da23803303235975f718ec2
Andrew Wong <andrew.j.wong@outlook.com>
2022-11-25 19:11:06 +1100

    :|

--- src/lib/Questionnaire.svelte
+++ src/lib/Questionnaire.svelte
@@ -104,189 +104,197 @@
   dayjs.extend(dayjs_relativeTime);
 </script>
 
-<div class="pb-8">
-  <h1>
-    <span>Should I buy a</span>
-    <input
-      class="minimalUnderline"
-      bind:value={options.itemName}
-      type="text"
-      placeholder="Enter the item here"
-      disabled={uiState.showResult || uiState.isCalculating}
-    />
-  </h1>
-</div>
+<div class="client">
+  <div class="pb-8">
+    <h1>
+      <span>Should I buy a</span>
+      <input
+        class="minimalUnderline"
+        bind:value={options.itemName}
+        type="text"
+        placeholder="Enter the item here"
+        disabled={uiState.showResult || uiState.isCalculating}
+      />
+    </h1>
+  </div>
 
-{#if options.itemName && uiState.showOptions}
-  <Question>
-    <div class="form-control" transition:fade>
-      <label class="label">
-        <span class="label-text">Do you need it?</span>
-      </label>
-      <label class="label cursor-pointer">
-        <input
-          type="radio"
-          class="radio checked:bg-green-500"
-          bind:group={options.need}
-          value={true}
-        />
-        <span class="label-text">Yes</span>
-      </label>
-    </div>
-    <div class="form-control">
-      <label class="label cursor-pointer">
-        <input
-          type="radio"
-          class="radio checked:bg-red-500"
-          bind:group={options.need}
-          value={false}
-        />
-        <span class="label-text">No</span>
-      </label>
-    </div>
-  </Question>
-
-  {#if options.need === false}
+  {#if options.itemName && uiState.showOptions}
     <Question>
       <div class="form-control" transition:fade>
         <label class="label">
-          <span class="label-text">But do you want it?</span>
+          <span class="label-text">Do you need it?</span>
         </label>
         <label class="label cursor-pointer">
           <input
             type="radio"
             class="radio checked:bg-green-500"
-            bind:group={options.want}
+            bind:group={options.need}
             value={true}
           />
           <span class="label-text">Yes</span>
         </label>
       </div>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <input
+            type="radio"
+            class="radio checked:bg-red-500"
+            bind:group={options.need}
+            value={false}
+          />
+          <span class="label-text">No</span>
+        </label>
+      </div>
+    </Question>
+
+    {#if options.need === false}
+      <Question>
+        <div class="form-control" transition:fade>
+          <label class="label">
+            <span class="label-text">But do you want it?</span>
+          </label>
+          <label class="label cursor-pointer">
+            <input
+              type="radio"
+              class="radio checked:bg-green-500"
+              bind:group={options.want}
+              value={true}
+            />
+            <span class="label-text">Yes</span>
+          </label>
+        </div>
+      </Question>
+    {/if}
+
+    <Question>
+      <div class="form-control" transition:fade>
+        <label class="label">
+          <span class="label-text">How much is it?</span>
+        </label>
+        <div class="input-group">
+          <select
+            class="select select-bordered"
+            bind:value={uiState.currencyUnit}
+          >
+            {#each ["$", "€", "£", "¥"] as t}
+              <option>{t}</option>
+            {/each}
+          </select>
+          <input
+            type="number"
+            min="0"
+            placeholder="Price"
+            class="input input-bordered w-full max-w-xs"
+          />
+        </div>
+      </div>
+    </Question>
+
+    <Question>
+      <div class="form-control" transition:fade>
+        <label class="label">
+          <span class="label-text">Is it on sale?</span>
+        </label>
+        <label class="label cursor-pointer">
+          <input
+            type="radio"
+            class="radio checked:bg-green-500"
+            bind:group={options.sale}
+            value={true}
+          />
+          <span class="label-text">Yes</span>
+        </label>
+      </div>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <input
+            type="radio"
+            class="radio checked:bg-red-500"
+            bind:group={options.sale}
+            value={false}
+          />
+          <span class="label-text">No</span>
+        </label>
+      </div>
+    </Question>
+
+    <Question>
+      <div class="form-control" transition:fade>
+        <label class="label">
+          <span class="label-text">What's your income?</span>
+        </label>
+        <div class="input-group">
+          <select
+            class="select select-bordered"
+            bind:value={uiState.currencyUnit}
+          >
+            {#each ["$", "€", "£", "¥"] as t}
+              <option>{t}</option>
+            {/each}
+          </select>
+          <input
+            type="number"
+            min="0"
+            placeholder="Income"
+            class="input input-bordered w-full max-w-xs"
+            bind:value={options.income.value}
+          />
+          <select
+            class="select select-bordered"
+            bind:value={options.income.unit}
+          >
+            <option value="hr">per hour</option>
+            <option value="yr">per year</option>
+          </select>
+        </div>
+      </div>
     </Question>
   {/if}
 
-  <Question>
-    <div class="form-control" transition:fade>
-      <label class="label">
-        <span class="label-text">How much is it?</span>
-      </label>
-      <div class="input-group">
-        <select
-          class="select select-bordered"
-          bind:value={uiState.currencyUnit}
-        >
-          {#each ["$", "€", "£", "¥"] as t}
-            <option>{t}</option>
-          {/each}
-        </select>
-        <input
-          type="number"
-          min="0"
-          placeholder="Price"
-          class="input input-bordered w-full max-w-xs"
-        />
-      </div>
-    </div>
-  </Question>
+  {#if options.itemName}
+    {#if uiState.isCalculating || uiState.showOptions !== false}
+      <button
+        transition:fade
+        class="btn btn-lg"
+        class:loading={uiState.isCalculating}
+        on:click={shouldIBuyIt}
+        >{#if !uiState.isCalculating}Should I buy it{/if}</button
+      >
+    {/if}
 
-  <Question>
-    <div class="form-control" transition:fade>
-      <label class="label">
-        <span class="label-text">Is it on sale?</span>
-      </label>
-      <label class="label cursor-pointer">
-        <input
-          type="radio"
-          class="radio checked:bg-green-500"
-          bind:group={options.sale}
-          value={true}
-        />
-        <span class="label-text">Yes</span>
-      </label>
-    </div>
-    <div class="form-control">
-      <label class="label cursor-pointer">
-        <input
-          type="radio"
-          class="radio checked:bg-red-500"
-          bind:group={options.sale}
-          value={false}
-        />
-        <span class="label-text">No</span>
-      </label>
-    </div>
-  </Question>
-
-  <Question>
-    <div class="form-control" transition:fade>
-      <label class="label">
-        <span class="label-text">What's your income?</span>
-      </label>
-      <div class="input-group">
-        <select
-          class="select select-bordered"
-          bind:value={uiState.currencyUnit}
-        >
-          {#each ["$", "€", "£", "¥"] as t}
-            <option>{t}</option>
-          {/each}
-        </select>
-        <input
-          type="number"
-          min="0"
-          placeholder="Income"
-          class="input input-bordered w-full max-w-xs"
-          bind:value={options.income.value}
-        />
-        <select class="select select-bordered" bind:value={options.income.unit}>
-          <option value="hr">per hour</option>
-          <option value="yr">per year</option>
-        </select>
-      </div>
-    </div>
-  </Question>
-{/if}
-
-{#if options.itemName}
-  {#if uiState.isCalculating || uiState.showOptions !== false}
-    <button
-      transition:fade
-      class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-      class:loading={uiState.isCalculating}
-      on:click={shouldIBuyIt}
-      >{#if !uiState.isCalculating}Should I buy it{/if}</button
-    >
+    {#if uiState.showOptions === null}
+      <button
+        class="btn btn-outline btn-sm md:btn-md lg:btn-lg"
+        on:click={() => (uiState.showOptions = true)}>Refine AI</button
+      >
+    {/if}
   {/if}
 
-  {#if uiState.showOptions === null}
-    <button
-      class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
-      on:click={() => (uiState.showOptions = true)}>Refine AI</button
-    >
+  {#if uiState.showResult}
+    <div transition:fly={{ delay: 800, y: 300 }} class="mt-8">
+      <h1>Yes.</h1>
+
+      {#key uiState.elapsedTime}
+        {#if uiState.elapsedTime && options.income.value}
+          <p>
+            This tool just took {dayjs
+              .duration(uiState.elapsedTime, "s")
+              .humanize()} of your life away.
+          </p>
+          <p>
+            That's roughly {uiState.currencyUnit}{getMoneyWasted()} you could have
+            earned to put towards this purchase
+          </p>
+        {/if}
+      {/key}
+    </div>
   {/if}
-{/if}
-
-{#if uiState.showResult}
-  <div transition:fly={{ delay: 800, y: 300 }} class="mt-8">
-    <h1>Yes</h1>
-
-    {#key uiState.elapsedTime}
-      {#if uiState.elapsedTime && options.income.value}
-        <p>
-          This tool just took {dayjs
-            .duration(uiState.elapsedTime, "s")
-            .humanize()} of your life away.
-        </p>
-        <p>
-          That's roughly {uiState.currencyUnit}{getMoneyWasted()} you could have
-          earned to put towards this purchase
-        </p>
-      {/if}
-    {/key}
-  </div>
-{/if}
+</div>
 
 <style lang="scss">
+  .client {
+    text-align: center;
+  }
   .minimalUnderline {
     text-align: center;
 
